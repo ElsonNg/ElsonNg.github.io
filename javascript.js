44e79acd58eba306ed8284a24fcc0802a3c72218
Elson Ng <42328403+ComputerManiiac@users.noreply.github.com>
2020-05-29 12:27:07 +0800

    Test

--- javascript.js
+++ javascript.js
@@ -14,7 +14,3 @@ $(".content-toolbar a").on('click', function(){
     return false;
 });
 
-var path = window.location.pathname;
-var page = path.split("/").pop();
-if(page == "index.html" || page == "contact.html")
-    $('body,html').css('overflow','hidden');
